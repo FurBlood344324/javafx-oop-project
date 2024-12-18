a0c0117804af967f45151e7e2c4c6b06dd98fc48
Furkan Cevik <furkancevik181@gmail.com>
2024-12-19 01:08:41 +0300

    Updated AdminSceneController.java

--- src/main/java/com/example/bagistakipsistemi/Controllers/AdminManageSceneController.java
+++ src/main/java/com/example/bagistakipsistemi/Controllers/AdminManageSceneController.java
@@ -1,38 +1,7 @@
 package com.example.bagistakipsistemi.Controllers;
 
 import com.example.bagistakipsistemi.Classes.*;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 
-import java.awt.event.ActionEvent;
-import java.net.URL;
-import java.util.ResourceBundle;
+public class AdminManageSceneController {
 
-public class AdminManageSceneController implements Initializable {
-    @FXML
-    private Label myTitleLabel;
-
-    @FXML
-    private ListView<String> myListView;
-
-    @FXML
-    private Button myButton1, myButton2;
-
-    private String[] str = {"1", "2", "3"};
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        myListView.getItems().addAll(str);
-    }
-
-    public void fonk(ActionEvent actionEvent) {
-        System.out.println("fonk");
-    }
-
-    public void switchtoMainScene1(ActionEvent actionEvent) {
-
-    }
 }
